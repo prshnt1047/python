53cc64a778ee378c44b0978fd8e4cecf194ef0b1
prshnt1047 <44678015+prshnt1047@users.noreply.github.com>
2018-11-02 16:52:09 +0530

    Add files via upload

---
+++ area of rectangle.py
@@ -0,0 +1,15 @@
+l=12
+b=8
+a=l*b
+print("area of rectangle",a);
+    
+d=2
+r=d/2
+print("radius of circle",r);
+
+option1=input ('radius of circle')
+option2=input ('area of rectangle')
+if option1:
+    print("radius of circle",r);
+else:
+    print("area of rectangle",a);
